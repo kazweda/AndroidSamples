31bd45dfa1e39cf2651b5aabf5e726fe58bb5d93
kazuaki.ueda@gmail.com <kazuaki.ueda@gmail.com>
2012-11-20 22:39:39 +0900

    First Commit

---
+++ MultiDensity/src/jp/netplan/android/multidensity/MainActivity.java
@@ -0,0 +1,62 @@
+package jp.netplan.android.multidensity;
+
+import java.util.Calendar;
+
+import android.os.Bundle;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+// For more details, refer to next URI.
+// http://developer.android.com/intl/ja/guide/topics/ui/controls/pickers.html
+//
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public void showTimePickerDialog(View v) {
+	    DialogFragment newFragment = new DatePickerFragment();
+	    newFragment.show(getSupportFragmentManager(), "timePicker");
+	}
+	
+	public static class DatePickerFragment extends DialogFragment
+		implements OnDateSetListener {
+		
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+		    // Use the current time as the default values for the picker
+		    final Calendar c = Calendar.getInstance();
+		    int year = c.get(Calendar.YEAR);
+	        int month = c.get(Calendar.MONTH);
+	        int day = c.get(Calendar.DAY_OF_MONTH);
+
+	        // Create a new instance of DatePickerDialog and return it
+	        return new DatePickerDialog(getActivity(), this, year, month, day);
+	    }
+
+	    public void onDateSet(DatePicker view, int year, int month, int day) {
+	        // Do something with the date chosen by the user
+	    }
+	}
+	
+}
+
